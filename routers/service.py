31f45689829f8da63d8042a85263c20e04df7d9e
HUANG-CHI-RUEI <pei4b066@gmail.com>
2022-08-17 09:58:29 +0800

    second commit

--- routers/service.py
+++ routers/service.py
@@ -43,17 +43,17 @@ CLIENT = InfluxDBClient('127.0.0.1', 8086, 'root', 'password', 'testdb')
     
 #     return result
 
-@router.get("/read_temperature")
-def read_temparature():
-        sql = 'select * from Temperature'
+@router.get("/read_param")
+def read_param():
+        sql = 'select * from parameter'
         result = list(CLIENT.query(sql).get_points())
         return result
 
 
 
-@router.delete("/del_tempature", status_code=204)
+@router.delete("/del_param", status_code=204)
 def delete_temp():
-    drop_temp = CLIENT.query('delete from Temperature')
+    drop_temp = CLIENT.query('delete from parameter')
     return {"result": "delete completed!"}
 
 
@@ -63,12 +63,12 @@ def write_temperature():
     #溫度
     temperature = [
         {
-            "measurement": "Temperature",
+            "measurement": "parameter",
             "tags": {
-                "topic": "Sensor/Temperature"
+                "topic": "temperature"
             },
             "fields":{
-                "tem": value
+                "value": value
             }
         }
     ]
@@ -79,16 +79,16 @@ def write_temperature():
 
 @router.post("/write_phvalue")
 def write_phvalue():
-
+    value = random.randint(10,30)
     #酸鹼度
     phvalue = [
         {
-            "measurement": "Phvalue",
+            "measurement": "parameter",
             "tags": {
-                "topic": "Sensor/Phvalue"
+                "topic": "phvalue"
             },
             "fields":{
-                "ph": 6.5
+                "value": value
             }
         }
     ]
@@ -97,130 +97,156 @@ def write_phvalue():
     return {"result": "Done"}
 
 
-@router.post("/write_redoxpotential")
-def write_redoxpotential():
-    #氧化還原電位
-    redoxpotential = [
+@router.post("/write_atp")
+def write_temperature():
+    value = random.randint(10,100)
+    
+    atp_param = [
         {
-            "measurement": "Redox",
+            "measurement": "parameter",
             "tags": {
-                "topic": "Sensor/Redox"
+                "topic": "atp_details",
             },
             "fields":{
-                "redox": 45
+                "atp":"151",
+                "atp_hour": "30",
+                "pH": "7.5",
+                "turbidity":"0.043",
+                "orp":"9",
+                "temperature":"21",
+                "water":"1330",
+                "uvp":"230"
             }
         }
     ]
-    CLIENT.write_points(redoxpotential)
+    CLIENT.write_points(atp_param)
+
     return {"result": "Done"}
 
-@router.post("/write_turbidity")
-def write_turbidity():
-    #濁度
-    turbidity = [
-        {
-            "measurement": "Turbidity",
-            "tags": {
-                "topic": "Sensor/Turbidity"
-            },
-            "fields":{
-                "redox": 17
-            }
-        }
-    ]
-    CLIENT.write_points(turbidity)
-   
-    return {"result": "Done"}
+# @router.post("/write_redoxpotential")
+# def write_redoxpotential():
+#     #氧化還原電位
+#     redoxpotential = [
+#         {
+#             "measurement": "Redox",
+#             "tags": {
+#                 "topic": "Sensor/Redox"
+#             },
+#             "fields":{
+#                 "redox": 45
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(redoxpotential)
+#     return {"result": "Done"}
 
-@router.post("/write_circulatingflow")
-def write_circulatingflow():
-    # 循環流量
-    circulatingflow = [
-        {
-            "measurement": "CirculatingFlow",
-            "tags": {
-                "topic": "Sensor/circulatingflow"
-            },
-            "fields":{
-                "cf": 123
-            }
-        }
-    ]
-    CLIENT.write_points(circulatingflow)
+# @router.post("/write_turbidity")
+# def write_turbidity():
+#     #濁度
+#     turbidity = [
+#         {
+#             "measurement": "Turbidity",
+#             "tags": {
+#                 "topic": "Sensor/Turbidity"
+#             },
+#             "fields":{
+#                 "redox": 17
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(turbidity)
    
-    return {"result": "Done"}
+#     return {"result": "Done"}
 
-@router.post("/write_uvpower")
-def write_uvpower():
-    # UV功率
-    uvpower = [
-        {
-            "measurement": "UVPower",
-            "tags": {
-                "topic": "Sensor/UVPower"
-            },
-            "fields":{
-                "uvp": 63
-            }
-        }
-    ]
-    CLIENT.write_points(uvpower)
+# @router.post("/write_circulatingflow")
+# def write_circulatingflow():
+#     # 循環流量
+#     circulatingflow = [
+#         {
+#             "measurement": "CirculatingFlow",
+#             "tags": {
+#                 "topic": "Sensor/circulatingflow"
+#             },
+#             "fields":{
+#                 "cf": 123
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(circulatingflow)
    
-    return {"result": "Done"}
+#     return {"result": "Done"}
 
-@router.post("/write_uvtime")
-def write_uvtime():
-    # UV使用時數
-    uvtime = [
-        {
-            "measurement": "UVTime",
-            "tags": {
-                "topic": "Sensor/UVTime"
-            },
-            "fields":{
-                "uvt": 8.9
-            }
-        }
-    ]
-    CLIENT.write_points(uvtime)
+# @router.post("/write_uvpower")
+# def write_uvpower():
+#     # UV功率
+#     uvpower = [
+#         {
+#             "measurement": "UVPower",
+#             "tags": {
+#                 "topic": "Sensor/UVPower"
+#             },
+#             "fields":{
+#                 "uvp": 63
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(uvpower)
    
-    return {"result": "Done"}
+#     return {"result": "Done"}
 
-@router.post("/write_uvtime")
-def write_uvtime():
-    # UV使用時數
-    uvtime = [
-        {
-            "measurement": "UVTime",
-            "tags": {
-                "topic": "Sensor/UVTime"
-            },
-            "fields":{
-                "uvt": 8.9
-            }
-        }
-    ]
-    CLIENT.write_points(uvtime)
+# @router.post("/write_uvtime")
+# def write_uvtime():
+#     # UV使用時數
+#     uvtime = [
+#         {
+#             "measurement": "UVTime",
+#             "tags": {
+#                 "topic": "Sensor/UVTime"
+#             },
+#             "fields":{
+#                 "uvt": 8.9
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(uvtime)
    
-    return {"result": "Done"}
+#     return {"result": "Done"}
 
-@router.post("/write_colony")
-def write_colony():
-    # 菌落數
-    colony = [
-        {
-            "measurement": "Colony",
-            "tags": {
-                "topic": "Sensor/Colony"
-            },
-            "fields":{
-                "count": 50897
-            }
-        }
-    ]
-    CLIENT.write_points(colony)
+# @router.post("/write_uvtime")
+# def write_uvtime():
+#     # UV使用時數
+#     uvtime = [
+#         {
+#             "measurement": "UVTime",
+#             "tags": {
+#                 "topic": "Sensor/UVTime"
+#             },
+#             "fields":{
+#                 "uvt": 8.9
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(uvtime)
    
-    return {"result": "Done"}
+#     return {"result": "Done"}
+
+# @router.post("/write_colony")
+# def write_colony():
+#     # 菌落數
+#     colony = [
+#         {
+#             "measurement": "Colony",
+#             "tags": {
+#                 "topic": "Sensor/Colony"
+#             },
+#             "fields":{
+#                 "count": 50897
+#             }
+#         }
+#     ]
+#     CLIENT.write_points(colony)
+   
+#     return {"result": "Done"}
 
 
 
